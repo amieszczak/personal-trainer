3fead542c3f52ac34ef665fd9f263ae7a36c01dc
ADRIAN MIESZCZAK <mieszczak22@gmail.com>
2025-12-05 19:34:45 +0100

    app reorganization

---
+++ frontend/.angular/cache/19.2.19/personal-trainer/vite/deps/@angular_cdk_text-field.js
@@ -0,0 +1,16 @@
+import {
+  AutofillMonitor,
+  CdkAutofill,
+  CdkTextareaAutosize,
+  TextFieldModule
+} from "./chunk-E5Q3CSLV.js";
+import "./chunk-6NF7AONN.js";
+import "./chunk-BM3LF3QA.js";
+import "./chunk-G3R3A7LM.js";
+import "./chunk-K5NYMLMM.js";
+export {
+  AutofillMonitor,
+  CdkAutofill,
+  CdkTextareaAutosize,
+  TextFieldModule
+};
---
+++ frontend/.angular/cache/19.2.19/personal-trainer/vite/deps/chunk-4UWO4I76.js
@@ -0,0 +1,46 @@
+// node_modules/@angular/cdk/fesm2022/boolean-property-DaaVhX5A.mjs
+function coerceBooleanProperty(value) {
+  return value != null && `${value}` !== "false";
+}
+
+// node_modules/@angular/cdk/fesm2022/css-pixel-value-C_HEqLhI.mjs
+function coerceCssPixelValue(value) {
+  if (value == null) {
+    return "";
+  }
+  return typeof value === "string" ? value : `${value}px`;
+}
+
+// node_modules/@angular/cdk/fesm2022/coercion.mjs
+function coerceStringArray(value, separator = /\s+/) {
+  const result = [];
+  if (value != null) {
+    const sourceValues = Array.isArray(value) ? value : `${value}`.split(separator);
+    for (const sourceValue of sourceValues) {
+      const trimmedString = `${sourceValue}`.trim();
+      if (trimmedString) {
+        result.push(trimmedString);
+      }
+    }
+  }
+  return result;
+}
+
+// node_modules/@angular/cdk/fesm2022/test-environment-CT0XxPyp.mjs
+function _isTestEnvironment() {
+  return (
+    // @ts-ignore
+    typeof __karma__ !== "undefined" && !!__karma__ || // @ts-ignore
+    typeof jasmine !== "undefined" && !!jasmine || // @ts-ignore
+    typeof jest !== "undefined" && !!jest || // @ts-ignore
+    typeof Mocha !== "undefined" && !!Mocha
+  );
+}
+
+export {
+  _isTestEnvironment,
+  coerceBooleanProperty,
+  coerceCssPixelValue,
+  coerceStringArray
+};
+//# sourceMappingURL=chunk-4UWO4I76.js.map
